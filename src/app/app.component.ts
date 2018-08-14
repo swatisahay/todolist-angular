7a744760d5177a7a435098a76598c29db9472304
Colin Bates, Nick Rogers and Swati Sahay <nick@>
2018-08-14 15:37:21 -0700

    created todo list using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
+  selectedTask = null;
+
+  editTask(clickedTask) {
+      this.selectedTask = clickedTask;
+    }
+    finishedEditing() {
+    this.selectedTask = null;
+  }
+  priorityColor(currentTask){
+   if (currentTask.priority === 3){
+     return "bg-danger";
+   } else if (currentTask.priority === 2) {
+     return  "bg-warning";
+   } else {
+     return "bg-info";
+   }
+ }
+
 }
